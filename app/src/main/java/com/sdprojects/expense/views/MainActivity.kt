36d19db78f9fe46755b6ed8f671c976c9105427c
Saptarshi Das <babatezpur@gmail.com>
2023-09-16 21:41:58 +0530

    Create packages and add button

---
+++ app/src/main/java/com/sdprojects/expense/views/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.sdprojects.expense.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import com.sdprojects.expense.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_activity_menu, menu)
+        return true
+    }
+}
